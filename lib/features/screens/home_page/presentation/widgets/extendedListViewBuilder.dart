7bbc5f36a0a033c239ef4492e40998f4bcde1cca
masnhebpearls <mansh.ebpearls@gmail.com>
2024-06-30 12:05:37 +0545

    minor update

--- lib/features/screens/home_page/presentation/widgets/extendedListViewBuilder.dart
+++ lib/features/screens/home_page/presentation/widgets/extendedListViewBuilder.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
-import 'package:news_app/config/themes/styles.dart';
 import 'package:news_app/features/screens/home_page/presentation/bloc/news_bloc.dart';
 import '../../models/news_model/news_model.dart';
 import 'news_card.dart';
@@ -12,9 +10,11 @@ class ExtendedListViewBuilder extends StatefulWidget {
     super.key,
     required this.model,
     required this.isNewsView,
+    required this.hasInternetConnection
   });
 
   final bool isNewsView;
+  final bool hasInternetConnection;
   final List<NewsModel> model;
 
   @override
@@ -23,43 +23,21 @@ class ExtendedListViewBuilder extends StatefulWidget {
 
 // Make the state class public by removing the underscore
 class ExtendedListViewBuilderState extends State<ExtendedListViewBuilder> {
-  bool? hasInternetConnection;
 
   final GlobalKey<LiquidPullToRefreshState> _refreshIndicatorKey =
       GlobalKey<LiquidPullToRefreshState>();
 
-  @override
-  void initState() {
-    super.initState();
-    _checkInternetConnection();
-  }
-
-  Future<void> _checkInternetConnection() async {
-    final hasInternet = await InternetConnection().hasInternetAccess;
-    setState(() {
-      print("connection in root is $hasInternetConnection");
-      hasInternetConnection = hasInternet;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    if (hasInternetConnection == null) {
-      // Show a loading indicator while checking for internet connectivity
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
+
 
     return BlocBuilder<NewsBloc, NewsState>(
       builder: (context, state) {
         return LiquidPullToRefresh(
           key: _refreshIndicatorKey,
           onRefresh: () async {
-            await _checkInternetConnection();
-            if (hasInternetConnection!) {
-              context.read<NewsBloc>().add(ApiRequestEvent());
-            }
+           context.read<NewsBloc>().add(CheckInternetConnection());
           },
           child: widget.model.isNotEmpty
               ? ListView.builder(
@@ -70,7 +48,7 @@ class ExtendedListViewBuilderState extends State<ExtendedListViewBuilder> {
                         .hiveSavedNews
                         .contains(widget.model[index]);
                     return NewsCard(
-                      hasInternetConnection: hasInternetConnection!,
+                      hasInternetConnection: widget.hasInternetConnection,
                       isNewsView: widget.isNewsView,
                       model: widget.model[index],
                       isSavedView: isSavedView,
